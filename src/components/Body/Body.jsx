44353cd6f06b99e8d25fabbc3ac43ee83ab48f0b
CloudStone <92814911+CloudStoned@users.noreply.github.com>
2024-03-07 13:16:03 +0800

    Enhance: Container 3

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -3,8 +3,6 @@ import './Body.css';
 import leaf1 from '../assets/leaf-balayong.jpg';
 import leaf2 from '../assets/leaf-bayabas.png';
 
-
-
 export function DictionaryBody() {
     return (
         <div className="wrapper">
@@ -20,14 +18,14 @@ export function DictionaryBody() {
 
                 <div className="paragraphs">
                     <div className="paragraph-1">
-                        <p>1. a printed sheet of paper, 
-                            sometimes folded, containing <strong><span style={{ color: "green" }}>information</span></strong> or 
+                        <p>1. a printed sheet of paper,
+                            sometimes folded, containing <strong><span style={{ color: "green" }}>information</span></strong> or
                             <br></br>advertising and usually distributed free</p>
                     </div>
 
                     <div className="paragraph-2">
                         <p>
-                        2. Each of the leaflike structures that together make up a compound leaf.
+                            2. Each of the leaflike structures that together make up a compound leaf.
                         </p>
                     </div>
                 </div>
@@ -40,32 +38,29 @@ export function DictionaryBody() {
 
                 <div className="paragraphs">
                     <p>Found just behind the College Promenade and Mater et Magistra's blessed gaze, the zen-inspired Serenity Garden which can be visited by students
-                    and staffs alike to encourage contemplation and reflection to those experience its peacefulness. Integrating FAITH Colleges' belief that students learn in three ways, that of which: through classroom lessons; through integration of values in 
+                    and staffs alike to encourage contemplation and reflection to those experience its peacefulness. Integrating FAITH Colleges' belief that students learn in three ways, that of which: through classroom lessons; through integration of values in
                     various subjects; and most importantly, through the environment. The Serenity Garden exudes a strong sense of order, suggesting that everything has a natural order and a natural rule.
                     </p>
-                    
+
                 </div>
             </div>
 
             <div className="leaves-container">
-                <img src={leaf1} alt="Leaf Image" className="leaf-pic-size" />
-                <img src={leaf2} alt="Leaf Image" className="leaf-pic-size" />
-                <img src={leaf2} alt="Leaf Image" className="leaf-pic-size" />
-                <img src={leaf2} alt="Leaf Image" className="leaf-pic-size" />
-                <img src={leaf2} alt="Leaf Image" className="leaf-pic-size" />
-                <img src={leaf2} alt="Leaf Image" className="leaf-pic-size" />
-                <img src={leaf2} alt="Leaf Image" className="leaf-pic-size" />
-                <img src={leaf2} alt="Leaf Image" className="leaf-pic-size" />
-                <img src={leaf2} alt="Leaf Image" className="leaf-pic-size" />
-                <img src={leaf2} alt="Leaf Image" className="leaf-pic-size" />
+                <div className="leaf-text">
+                    <img src={leaf1} alt="Leaf Image" className="leaf-pic-size" />
+                    <div className="leaf-name">
+                        <p>Dita</p>
+                    </div>
+                </div>
 
+                <div className="leaf-text">
+                    <img src={leaf2} alt="Leaf Image" className="leaf-pic-size" />
+                    <div className="leaf-name">
+                        <p>Guyabano</p>
+                    </div>
+                </div>
 
             </div>
-
-
-
-
-
 
         </div>
     );
