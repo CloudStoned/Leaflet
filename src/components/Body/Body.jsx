a12a580fd989675d55b69f7550b363b37e243b92
CloudStone <92814911+CloudStoned@users.noreply.github.com>
2024-03-07 10:54:13 +0800

    Added: Second Container

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -8,7 +8,7 @@ export function DictionaryBody() {
                 <h1> DICTIONARY</h1>
             </div>
 
-            <div className="container-1">
+            <div className="containers">
 
                 <div className="leaflet-title">
                     <h2>Leaflet</h2>
@@ -28,6 +28,21 @@ export function DictionaryBody() {
                     </div>
                 </div>
             </div>
+
+            <div className="containers">
+                <div className="serenity-title">
+                    <h2>Serenetiy Garden</h2>
+                </div>
+
+                <div className="paragraphs">
+                    <p>Found just behind the College Promenade and Mater et Magistra's blessed gaze, the zen-inspired Serenity Garden which can be visited by students
+                    and staffs alike to encourage contemplation and reflection to those experience its peacefulness. Integrating FAITH Colleges' belief that students learn in three ways, that of which: through classroom lessons; through integration of values in 
+                    various subjects; and most importantly, through the environment. The Serenity Garden exudes a strong sense of order, suggesting that everything has a natural order and a natural rule.
+                    </p>
+                    
+                </div>
+            </div>
+            
         </div>
     );
 }
